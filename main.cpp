97cd887413dfa11ce3cba47b0d66e9651afd4d94
Ivan Erofeev <ivanerofeev01@gmail.com>
2024-12-09 19:01:30 +0300

    added dev tools

--- main.cpp
+++ main.cpp
@@ -1,18 +1,23 @@
-
 #include <string>
 #include <iostream>
+#include <cstdint>
 
 int main() {
+    // Welcome to the game
     while(true) {
         std::string cmd;
         std::getline(std::cin, cmd);
 
         if(cmd == "exit") {
-            return 0;
+            std::cout << "ok" << std::endl;
+            break;
         } else if(cmd == "ping") {
             std::cout << "pong" << std::endl;
+        } else {
+            std::cout << "'" << cmd << "' is an unknown command!" 
+                      << " Please enter correct command." << std::endl;
         }
     }
 
-    return 0;
+    return EXIT_SUCCESS;
 }
